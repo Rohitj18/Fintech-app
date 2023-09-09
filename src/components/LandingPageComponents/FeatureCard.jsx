871307546dcb5ec40d550ddf2c30b6400cdfb2dc
Rohitj18 <rohit.jabade18@gmail.com>
2023-09-10 01:15:59 +0530

    added feature section(4th section) of homepage

--- src/components/LandingPageComponents/FeatureCard.jsx
+++ src/components/LandingPageComponents/FeatureCard.jsx
@@ -1,11 +1,55 @@
 import React from 'react'
+import stockvideo from '../../assets/LandingPageAssests/stockvideo.mp4'
+import wealthvideo from '../../assets/LandingPageAssests/wealthmanagement.mp4'
+import walletvideo from '../../assets/LandingPageAssests/personalizedwallet.mp4'
 
-const FeatureCard = ({text}) => {
+
+const data = [
+  {
+    heading:"Stock Market Analysis",
+    description:"Our website offers a cutting-edge Real-time Market Sentiment Analysis feature that empowers investors and traders with a comprehensive understanding of market sentiment dynamics."
+  },
+  {
+    heading:"Wealth Managment",
+    description:"Our website offers an advanced Personalized Financial Planning Dashboard, a robust feature designed to empower users with a comprehensive, tailored approach to managing their wealth."
+  },
+  {
+    heading:"Adv Digital Wallet",
+    description:'Introducing our "One-Click Secure Payments" feature for your digital wallet. This streamlined solution simplifies online transactions, prioritizes security, and enhances your overall convenience.'
+  }
+]
+
+const FeatureCard = ({isleft,index}) => {
+  let videoArr = [stockvideo,wealthvideo,walletvideo];
   return (
-    <div className='glass-effect2 w-[70%] h-[12%] rounded-lg flex justify-center items-center transition-all duration-300 ease-in-out hover:bg-white'>
-      <p className='text-semibol text-4xl'>{text}</p>
+    <div className={`flex ${isleft?"flex-row pr-[3.5em]":"flex-row-reverse pl-[3.5em]"} w-[100%] h-[100%] bg-yellow-400`}>
+    {/* left section */}
+      <div className='w-[65%] h-[100%] p-[3em] '>
+        <div className='flex flex-col'>
+          <p className='font-semibold text-6xl'>{data[index]?.heading}</p>
+          <div className='w-[50%] h-[0.2em] bg-black mt-[1.5em]'></div>
+          {/* description */}
+          <div className='flex flex-row h-[10em] mt-[3em] gap-5'>
+            <div className='h-[100%] w-[3%] bg-blue-500'>
+            </div>
+            <div>
+              <p className='text-3xl'>{data[index]?.description}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    {/* right section */}
+      <div className='w-[35%] h-[100%] flex items-center justify-center my-auto'>
+        <video muted loop autoPlay className='rounded-md shadow-[10.0px_14.0px_10.0px_rgba(0,0,0,0.38)]'> <source src={videoArr[index]} /></video>
+      </div>
     </div>
+    
   )
 }
 
 export default FeatureCard
+
+
+{/* <div className='glass-effect2 w-[70%] h-[12%] rounded-lg flex justify-center items-center transition-all duration-300 ease-in-out hover:bg-white'>
+      <p className='text-semibol text-4xl'>{text}</p>
+    </div> */}
--- src/components/LandingPageComponents/FourthSection.jsx
+++ src/components/LandingPageComponents/FourthSection.jsx
@@ -1,25 +1,37 @@
 import React from 'react'
 import FeatureCard from './FeatureCard'
+import { useEffect } from 'react';
+import Aos from 'aos';
+import "aos/dist/aos.css"
 
 const FourthSection = () => {
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
+}, [])
   return (
-    <div className='w-[100%] h-[70vh] '>
-      <div className='mx-auto flex flex-row h-[100%]'>
-        {/* left section */}
-        <div className='flex flex-col gap-[4rem] h-[100%] w-[50%] max-w-[50%] bg-primary-blue justify-center items-center'>
-            
-            <h2 className='text-5xl text-white font-bold'>Our Features</h2>   
-            <FeatureCard text={"Stock Analysis"}/>
-            <FeatureCard text={"Expense Management"}/>
-            <FeatureCard text={"Personalized Wallet"}/>
-            <FeatureCard text={"Porfolio Managment"}/>
-        </div>
-        {/* right section */}
-        <div className='flex gap-4 w-[50%] max-w-[50%] justify-center items-center'>
-            <div className='h-[100%] w-[80%]'>
-            </div>
+    <div className='w-[100%] flex flex-col gap-[5em] border-t-[0.2em] border-black border-solid pt-[5em]'>
+      <div className='flex flex-col justify-center items-center gap-6'>
+        <p className='font-semibold text-6xl text-ternary-color'>Discover Our Website's Power-Packed Features</p>
+        <div className='flex flex-row w-[100%] justify-center items-center gap-3'>
+          <div className='bg-black w-[5%] h-[0.4em]'></div>
+          <div className='bg-black w-[0.3%] h-[0.4em]'></div>
         </div>
       </div>
+      <div className='h-[100%] flex flex-col gap-[5em]'>
+          <div data-aos="fade-right" className='w-[60%] h-[45%]'>
+            <FeatureCard isleft={true} index={0}/>
+          </div>
+          <div data-aos="fade-left"className='w-[60%] h-[45%] flex self-end'>
+            <FeatureCard isleft={false} index={1}/>
+          </div>
+          <div data-aos="fade-right" className='w-[60%] h-[45%]'>
+            <FeatureCard isleft={true} index={2}/>
+          </div>
+          <div data-aos="fade-left"className='w-[60%] h-[45%] flex self-end'>
+            <FeatureCard isleft={false} index={1}/>
+          </div>
+      </div>
+
     </div>
   )
 }
