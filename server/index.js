4b193b1ee5276a7dcb0cd39b1396eb9ed5fb288d
Rohit Jabade <67982828+Rohitj18@users.noreply.github.com>
2023-10-30 23:35:36 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -31,6 +31,15 @@ app.use(
     })
 );
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
+    next();
+});
+
 app.use(
     fileUpload({
         useTempFiles:true,
